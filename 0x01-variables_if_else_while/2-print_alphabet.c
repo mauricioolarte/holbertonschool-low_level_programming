ef10494c7bc357aa8b56a735e5f6a6a563fccdfd
mauricioolarte <reneolarte@yahoo.com>
2020-02-06 22:11:14 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	putchar (b);
+	}
+	printf ("%s", "\n");
+	return (0);
+}
