be5f2ae7579a34e094ab920272c7c95c31b8d336
mauricioolarte <reneolarte@yahoo.com>
2020-02-06 23:16:58 +0000

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+	printf("%d", b);
+	}
+	putchar ('\n');
+	return (0);
+}
