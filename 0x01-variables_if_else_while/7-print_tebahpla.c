aaa44e459860973bbd80d7f375adf6349e187d30
mauricioolarte <reneolarte@yahoo.com>
2020-02-06 23:28:14 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'z'; b >= 'a'; b--)
+	{
+	putchar (b);
+	}
+	putchar ('\n');
+	return (0);
+}
