2473acbf766633060c4c89de2a72f8eae38e0d0c
mauricioolarte <reneolarte@yahoo.com>
2020-02-07 22:02:26 +0000

    1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+	int c;
+	int d;
+	int e;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			for (d = '0'; d <= '9'; d++)
+			{
+			for (e = '0'; e <= '9'; e++)
+			{
+			if (b <= d && c < e)
+			{
+			putchar (b);
+			putchar (c);
+			putchar (' ');
+			lputchar (d);
+			putchar (e);
+			if (b == '9' && c == '8' &&  d == '9' && e == '9')
+			{
+			break;
+			}
+			putchar (',');
+			putchar (' ');
+			 ('\n');
+	return (0);
+}
