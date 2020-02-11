5e2b2c08e5fde6a139a2f05e280d18d7655085de
mauricioolarte <reneolarte@yahoo.com>
2020-02-11 13:31:14 +0000

    23

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,26 @@
+#ifndef holberton.h
+#define holberton.h
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+int add(int n1, int n2);
+
+void print_to_98(int n);
+
+#endif /* holberton */
