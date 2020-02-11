0bd3da039703e4e019945ff0611f85341ef843e6
mauricioolarte <reneolarte@yahoo.com>
2020-02-11 21:14:12 +0000

    ds

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	intf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i); /* i never increase and ever is less than 10*/
-	} /* i = i++;*/
-
+/*
+*	while (i < 10)
+*	{
+*		putchar(i);
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
