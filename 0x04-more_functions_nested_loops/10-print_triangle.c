fcfe0e5d81a07190639016538841994308c99f47
mauricioolarte <reneolarte@yahoo.com>
2020-02-13 19:22:54 +0000

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *print_triangle - print diagonal line.
+ *
+ *@size: integer.
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+		_putchar(35);
+		}
+		_putchar('\n');
+
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+
+}
