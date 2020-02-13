013aab76aa4bd61878d91651ef58127427e9466b
mauricioolarte <reneolarte@yahoo.com>
2020-02-13 21:27:54 +0000

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *print_number - print a number whit _putchar.
+ *
+ *@n: is a integer.
+ *
+ */
+void print_number(int n)
+{
+	int i, d, k = 1, p = 1, j, larg;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	larg = n;
+
+	while (larg / 10 > 0)
+	{
+		larg = larg / 10;
+		k++;
+	}
+
+	for (i = k; i > 0; i--)
+	{
+		for (j = 2; j <= i; j++)
+		{
+			p = p * 10;
+		}
+
+		d = n / p;
+		_putchar(d + '0');
+		n = n - p * d;
+		p = 1;
+	}
+	_putchar('\n');
+}
