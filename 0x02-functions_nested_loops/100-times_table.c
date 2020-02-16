edf317305b9831f2bc797f1be3d8ffbbec1e3b07
mauricioolarte <reneolarte@yahoo.com>
2020-02-16 16:59:30 +0000

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,64 +1,41 @@
 #include "holberton.h"
 /**
- *times_table - print tables.
- *
+ *print_times_table - print tables.
  *Return: always 0.
- *
+ *@n: integer.
  */
 void print_times_table(int n)
 {
-	int a, b, d, u, i, c;
+	int num, cen, a, b, d, u, c;
 
 	if (!(n > 15 || n < 0))
 	{
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a < n; a++)
 		{
-			i = a * b;
-			c = i / 100;
-			d = (i - 100 * c) / 10;
-			u = i % 10;
-			if (b == 0)
+			for (b = 0; b < n; b++)
 			{
-				_putchar('0');
-				_putchar(',');
-			}
-			else if (i >= 0 && i <= 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(u + '0');
-				if (b != n)
+				num = a * b, cen = num / 100;
+				if (b == 0)
+					_putchar('0');
+				else
 				{
-				_putchar(',');
-				}
-			}
-			else if (i > 9 && i <= 99)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(d + '0');
-				_putchar(u + '0');
-				if (b != n)
+				if (num < 10)
+				u = num + '0', d = 32, c = 32;
+				if (num >= 10 && num < 100)
+				u = num % 10 + '0', d = num / 10 + '0', c = 32;
+				if (num > 99)
 				{
-					_putchar(',');
+				u = num % 10 + '0', d = (num % 100) / 10 + '0';
+				c = cen + '0';
 				}
-			}
-			else if (i > 99 && i <= 999)
-			{
-                                _putchar(' ');
-                                _putchar(c + '0');
-                                _putchar(d + '0');
-                                _putchar(u + '0');
-                                if (b != n)
-                                {
-                                        _putchar(',');
-                                }
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c);
+				_putchar(d);
+				_putchar(u);
+				}
 		}
+	_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
