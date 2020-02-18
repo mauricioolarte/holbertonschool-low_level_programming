b148ff3c7d2d85bc7e406213a19c456869dc4940
mauricioolarte <reneolarte@yahoo.com>
2020-02-18 23:09:54 +0000

    re

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", *(a + i));
 	}
-	printf("%c\n", *(a + n));
+	printf("%d\n", *(a + (n - 1)));
 }
