aa85dde02d0a44fe07436a416ccec5cda2f51ff3
mauricioolarte <reneolarte@yahoo.com>
2020-02-18 22:59:04 +0000

    8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*print_array - this print a array.
+*@a: array.
+*@n: size of array.
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d\n", *(a + n));
+}
