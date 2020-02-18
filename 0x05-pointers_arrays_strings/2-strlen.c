2ae75f0fd7664f74e8d2ffc1b6092cb0fecea9c6
mauricioolarte <reneolarte@yahoo.com>
2020-02-18 17:31:42 +0000

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *_strlen - retur the large of a string.
- *@s: is a direction of a sting.
- */
+*_strlen - retur the large of a string.
+*@s: is a direction of a sting.
+*Return: return i, number of character.
+*/
 int _strlen(char *s)
 {
 	int i;
