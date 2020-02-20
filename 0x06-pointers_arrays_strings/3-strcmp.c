f133dabe16a40f21682f3d46227d4249c1659430
mauricioolarte <reneolarte@yahoo.com>
2020-02-20 21:42:21 +0000

    ok

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*_strcmp - compare two string.
+*@s1: string.
+*@s2: string.
+*return: sum
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int sum, j, i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s1[j] == s2[j])
+		{
+			sum = 0;
+		}
+		else
+		{
+			sum = s1[j] - s2[j];
+			break;
+		}
+	}
+	return (sum);
+}
