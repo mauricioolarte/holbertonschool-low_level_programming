f6f74409d0da591d84602bbf1065460d5521b65b
mauricioolarte <reneolarte@yahoo.com>
2020-02-24 19:11:40 +0000

    fsaf

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j = 0;
 
-
-	while(j < n)
-		{
-			s[j] = b;
-			j++;
-		}
+	while (j < n)
+	{
+		s[j] = b;
+		j++;
+	}
 
 	return (s);
 }
