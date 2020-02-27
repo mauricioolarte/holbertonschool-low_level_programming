ac45f4507d521c4f7d1d8a7d2e6eeba95567b3b8
mauricioolarte <reneolarte@yahoo.com>
2020-02-27 21:10:32 +0000

    5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+*pot - potencia.
+*@a: integer.
+*@b: integer.
+*Return: -1 if y is negative.
+*/
+int pot(int a, int b)
+{
+	if (b < a)
+	{
+	if ( (b * b) != a)
+		return (pot(a, b + 1));
+	else
+		return (b);
+	}
+	else
+		return (-1);
+
+}
+
+/**
+*_sqrt_recursion - calculate a sqrt.
+*@n: integer.
+*Return: sqrt of n.
+ */
+
+int  _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (pot(n, 1));
+}
