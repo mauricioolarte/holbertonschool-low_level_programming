258d14cb6c093aef83f0f47374240f106831ec44
mauricioolarte <reneolarte@yahoo.com>
2020-02-27 20:32:19 +0000

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*_strlen_recursion - print string.
+*@s: string.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*(s + 0) != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+
+		}
+	return (0);
+}
