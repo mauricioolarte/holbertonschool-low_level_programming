f7901c167c357099a68673b35fc0181040d1567e
mauricioolarte <reneolarte@yahoo.com>
2020-03-01 18:38:39 -0500

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - calculate the numbers of coins.
+*@argc:is a integer.
+*@argv: is string.
+*Return: 0 when change is negative or 1 when there are two numbers.
+*/
+int main(int argc, char *argv[])
+{
+	int mon = 0, i = 0, change = atoi(argv[1]);
+	int coin[] = {25, 10, 5, 2, 1};
+
+	if (argc == 2)
+	{
+		if (change > 0)
+			{
+				while (i < 5)
+				{
+					if ((change / coin[i]) == 0)
+					{
+						i++;
+					}
+					else
+					{
+					mon += change / coin[i];
+					change = change % coin[i];
+					i++;
+					}
+				}
+				printf("%d\n", mon);
+			}
+			else
+			{
+				printf("0\n");
+			}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (mon);
+}
