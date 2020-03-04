16111be16c85537b7da92c96c61492c644b249d3
mauricioolarte <reneolarte@yahoo.com>
2020-03-03 23:25:56 -0500

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+***alloc_grid - this function return a pointer.
+*@width: this columns.
+*@height: this arrow.
+*Return: pointer.
+*/
+int char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, k = 0;
+	char **ar;
+
+	if (ac < 0)
+		return ('\0');
+
+	ar = malloc((ac) * sizeof(int *));
+	for (k = 0; k < ac; k++)
+	{
+		ar[k] = malloc(*av * sizeof(int));
+	}
+	for (i = 0; i < av; i++)
+	{
+		for (j = 0; j < ac; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+	return (ar);
+}
