68e59503437907b0797e70e838284aa3d87508b1
mauricioolarte <reneolarte@yahoo.com>
2020-03-03 22:27:30 -0500

    3-alloc_grid.c2

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+***alloc_grid - this function return a pointer.
+*@width: this columns.
+*@height: this arrow.
+*Return: pointer.
+*/
+int **alloc_grid(int *width, int *height)
+{
+	int i = 0, j = 0, k = 0;
+	int **ar;
+
+	ar = malloc((*height) * sizeof(int *));
+	for (k = 0; k < *height; k++)
+	{
+		ar[k] = malloc(*width * sizeof(int));
+			       }
+	for (i = 0; i < *height; i++)
+	{
+		for (j = 0; j < *width; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+	return(ar);
+}
