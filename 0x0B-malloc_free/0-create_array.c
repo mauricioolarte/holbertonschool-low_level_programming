1680af2ef36f031cabc3d0b187c35dfb54ccd351
mauricioolarte <reneolarte@yahoo.com>
2020-03-04 18:32:18 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return(NULL);
 	ar = malloc(size * sizeof(char));
-	if (malloc(size * sizeof(char)) == NULL)
+	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
