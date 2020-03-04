57db8bd484e8fcaf8bc17ce0e883a9b6e54cf963
mauricioolarte <reneolarte@yahoo.com>
2020-03-03 23:06:26 -0500

    3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,22 +5,22 @@
 *@height: this arrow.
 *Return: pointer.
 */
-int **alloc_grid(int *width, int *height)
+int **alloc_grid(int width, int height)
 {
 	int i = 0, j = 0, k = 0;
 	int **ar;
 
-	if (*width < 0 || *height < 0)
+	if (width < 0 || height < 0)
 		return ('\0');
 
-	ar = malloc((*height) * sizeof(int *));
-	for (k = 0; k < *height; k++)
+	ar = malloc((height) * sizeof(int *));
+	for (k = 0; k < height; k++)
 	{
-		ar[k] = malloc(*width * sizeof(int));
+		ar[k] = malloc(width * sizeof(int));
 	}
-	for (i = 0; i < *height; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < *width; j++)
+		for (j = 0; j < width; j++)
 		{
 			ar[i][j] = 0;
 		}
