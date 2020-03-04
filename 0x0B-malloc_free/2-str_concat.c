f0d704c747530ceb27b7686b6a979114149221e4
mauricioolarte <reneolarte@yahoo.com>
2020-03-04 18:56:02 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,8 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	unsigned int i = 0, j = 0, k = 0, h = 0;
 
+	if (*s1 == NULL || s2 == NULL)
+		return (NULL);
 	while (*(s1 + k) != '\0')
 		k++;
 	while (*(s2 + i) != '\0')
@@ -18,6 +20,8 @@ char *str_concat(char *s1, char *s2)
 	h = k + i;
 	i = 0;
 	ar = malloc((h + 1) * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
 	for (j = 0; j < k; j++)
 	{
 		ar[j] = *(s1 + j);
