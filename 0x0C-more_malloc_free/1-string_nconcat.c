389bbfd188062633d91a5302f8322dec1c4587a0
mauricioolarte <reneolarte@yahoo.com>
2020-03-05 18:09:26 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, k = 0, h = 0;
 
 	if (n < 0)
-		return(NULL);
+		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
