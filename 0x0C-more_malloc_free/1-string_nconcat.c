e2daab85a3befabe66795dc979937c055cf512e9
mauricioolarte <reneolarte@yahoo.com>
2020-03-05 18:14:18 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= i)
 		n = i;
 	n = k + n;
-	i = 0;
+	n = 0;
 	ar = malloc((h + 1) * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
