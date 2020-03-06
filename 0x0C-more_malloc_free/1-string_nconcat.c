b8c1acd614119681e2044ec24fec0d3f43041832
mauricioolarte <reneolarte@yahoo.com>
2020-03-05 23:05:16 -0500

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,26 +10,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i,  sum, k, h;
 	char *ar;
+
 	if (s1 == NULL)
-		h = 0;
-	else
-		for (h = 0; *(s1 + h); h++)
-			h++;
+		s1 = "";
+	for (h = 0; *(s1 + h); h++)
+		h++;
 	if (s2 == NULL)
-		k = 0;
-	else
-		for (k = 0; *(s2 + k); k++)
-			k++;
+		s2 = "";
+	for (k = 0; *(s2 + k); k++)
+		k++;
 	if (n >= k)
 		n = k;
 	sum = h + n + 1;
 	ar = malloc(sum * sizeof(char));
-	for (i =0; i < sum; i++)
+	for (i = 0; i < sum; i++)
 		ar[i] = '\0';
 	for (i = 0; i < h; i++)
 		ar[i] = s1[i];
 	for (i = h; i < (h + n); i++)
-		ar[i] = s2[(i-h)];
-
+		ar[i] = s2[(i - h)];
 	return (ar);
 }
