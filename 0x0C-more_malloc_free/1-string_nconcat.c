d4fda3cad2cc367a9432a84fbdf42c5023d64e41
mauricioolarte <reneolarte@yahoo.com>
2020-03-05 22:00:02 -0500

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 /**
 *string_nconcat - is a string.
 *@s1: is a string
@@ -10,30 +9,25 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ar;
-	unsigned int i = 0, j = 0, k = 0, h = 0;
+	unsigned int i = 0, sum, k = 0, h = 0;
 
-	h = strlen(s1);
-	k = strlen(s2);
+	for (h = 0; s1[h] != '\0'; h++)
+		h++;
+	if (s1 == NULL)
+		h = 0;
+	for (k = 0; s2[k] != '\0'; k++)
+		k++;
+	if (s2 == NULL)
+		k = 0;
 	if (n >= k)
 		n = k;
-	if (h = 0 || s1 == NULL)
-	{
-		ar = malloc((n + 1) * sizeof(char));
-		if (ar == NULL)
-			return (NULL);
-	}
-	if (k = 0 || s2 == NULL)
-	{
-		ar = malloc((h + 1) * sizeof(char));
-		if (ar == NULL)
-			return (NULL);
-	}
-	ar = malloc((k + h + 1) * sizeof(char));
+	sum = h + n + 1;
+	ar = malloc(sum * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < h; i++)
 		ar[i] = s1[i];
-	for (i = h; i < (h + k); i++)
+	for (i = h; i < (h + n); i++)
 		ar[i] = s2[(i-h)];
 	i++;
 	ar[i] = '\0';
