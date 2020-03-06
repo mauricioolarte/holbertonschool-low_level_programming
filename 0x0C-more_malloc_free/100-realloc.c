18e83dd6cde1979e457095b8d3a2ecd62331ed81
mauricioolarte <reneolarte@yahoo.com>
2020-03-06 21:07:03 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,10 +8,11 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
