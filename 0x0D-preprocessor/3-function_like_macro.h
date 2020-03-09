5a8761aacb3ed71c7d0effc39b60f4415f23e839
mauricioolarte <reneolarte@yahoo.com>
2020-03-09 17:36:33 +0000

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef _3_FUNCTION_LINE_H_
 #define _3_FUNCTION_LINE_H_
-#define ABS(x) (((x) > (0)) ? (x) : (-x))
+#define ABS(x) (((x) > (0)) ? (x) : ((x) * (-1)))
 #endif /* _3_FUNCTION_LINE_H */
