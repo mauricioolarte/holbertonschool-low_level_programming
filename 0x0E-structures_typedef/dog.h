4406e6431fe28d48d545242044f4a549c064ffb7
mauricioolarte <reneolarte@yahoo.com>
2020-03-09 19:50:00 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _1_DOGY_H_
+#define _1_DOGY_H_
+/**
+ *struct dog - is dog
+ *@name: is a name of dog.
+ *@age: is a age of dog.
+ *@owner: is name of owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /*_1_DOGY_H_*/
