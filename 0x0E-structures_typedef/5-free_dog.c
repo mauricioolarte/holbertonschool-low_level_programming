320de1f835a4ec280ff269bcb22c17cb79d0d1f2
mauricioolarte <reneolarte@yahoo.com>
2020-03-10 17:57:00 +0000

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *free_dog - free memory.
+ *@d: is pointer.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+	free(d->name);
+	free(d->owner);
+}
