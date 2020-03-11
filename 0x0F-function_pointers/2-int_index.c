9896c0a2afd84443061ecf4b6635b48cf70d90dd
mauricioolarte <reneolarte@yahoo.com>
2020-03-11 18:31:50 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-
+	if (array == NULL || cmp == NULL)
+		return (0);
 	if (size <= 0)
 		return (-1);
 	while (cmp(array[i]) != 1)
