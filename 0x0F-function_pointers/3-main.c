089eb32b422204612fabd93a0f7e79571ad34957
mauricioolarte <reneolarte@yahoo.com>
2020-03-11 22:35:00 +0000

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ *main - print op.
+ *@argc: is integer.
+ *@argv: is a array.
+ *Return: integer.
+ */
+
+int main(int argc, char *argv[])
+{
+	int r;
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	if (*argv[2] != '*' && *argv[2] != '+' && *argv[2] != '-' &&
+	    *argv[2] != '/' && *argv[2] != '%')
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+	if (atoi(&*argv[3]) == 0 && (*argv[2] != '%' || *argv[2] != '/'))
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	r = (*get_op_func(&*argv[2]))(atoi(&*argv[1]), atoi(&*argv[3]));
+	printf("%d\n", r);
+	return (0);
+}
