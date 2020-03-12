22c51a222593ae51bd464267807c0496cacd180e
mauricioolarte <reneolarte@yahoo.com>
2020-03-12 17:48:07 +0000

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '*' && *argv[2] != '+' && *argv[2] != '-' &&
-	    *argv[2] != '/' && *argv[2] != '%')
+	if ((*argv[2] != '*' && *argv[2] != '+' && *argv[2] != '-' &&
+	     *argv[2] != '/' && *argv[2] != '%') && (argv[2][1] == '\0'))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 	if (atoi(&*argv[3]) == 0 && (*argv[2] != '%' || *argv[2] != '/'))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(100);
 	}
 	r = (*get_op_func(&*argv[2]))(atoi(&*argv[1]), atoi(&*argv[3]));
