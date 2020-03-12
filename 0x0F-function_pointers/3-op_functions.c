1bf3275eb4557bfe0fd267804ea6891c9c904694
mauricioolarte <reneolarte@yahoo.com>
2020-03-12 17:38:59 +0000

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+B#include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
 /**
@@ -32,7 +32,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
@@ -43,7 +43,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
 
 /**
@@ -54,5 +54,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }
