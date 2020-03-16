bab33071de040f0683aeeec60f1ea90e2a5d3568
mauricioolarte <reneolarte@yahoo.com>
2020-03-16 02:41:32 +0000

    variadic_functions.h

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ *str - is print string
+ *@s: is a string.
+ */
+void str(char *s)
+{
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
+ *print_all - this function print everithing.
+ *@format: the format of argument.
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *separador = "";
+	va_list selector;
+
+	va_start(selector, format);
+	while (format[i] != '\0')
+	{
+		if (j > 0)
+			separador = ", ";
+
+		if (format[i] == 'c' || format[i] == 's' ||
+		    format[i] == 'f' || format[i] == 'i')
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", separador, va_arg(selector,
+								 int));
+				break;
+			case 's':
+				printf("%s", separador);
+				str(va_arg(selector, char *));
+				break;
+			case 'f':
+				printf("%s%f", separador,  va_arg(selector,
+								double));
+				break;
+			case 'i':
+				printf("%s%d", separador, va_arg(selector,
+							       int));
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(selector);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,5 @@ int int_index(int *array, int size, int (*cmp)(int));
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* _HOLBERTON_H_*/
