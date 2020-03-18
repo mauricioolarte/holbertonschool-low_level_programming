3f4c55e419d5ba20db1496c402898ee9cfed92ed
mauricioolarte <reneolarte@yahoo.com>
2020-03-18 19:42:26 +0000

    correct null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
-	if (n = 0)
+	if (n == 0)
 		printf("%s", separator);
 	va_start(argument, n);
 	for (i = 0; i < n; i++)
