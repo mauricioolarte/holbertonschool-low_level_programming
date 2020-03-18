cccec9dfbc009d505a26ebec1f1cccfbdb1820ea
mauricioolarte <reneolarte@yahoo.com>
2020-03-18 18:45:11 +0000

    variatric correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H_
 #define VARIADIC_FUNCTIONS_H_
-
+#include <stdarg.h>
 
 /**
- * struct selector - Struct 
+ * struct selector - Struct
  *
  * @p: The operator
- * @f: The function associated
+ * @fu: The function associated
  */
 typedef struct selector
 {
 	char *p;
-	void (*f)(void *);
+	void (*fu)(va_list selector);
 } imp;
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
@@ -19,4 +19,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _char(va_list selector);
+void _string(va_list selector);
+void _float(va_list selector);
+void _int(va_list selector);
 #endif /* _HOLBERTON_H_*/
