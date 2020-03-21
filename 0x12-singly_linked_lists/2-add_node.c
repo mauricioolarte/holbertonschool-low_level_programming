640fcadd5406f3af6b5a0400d7181b87c38ac753
mauricioolarte <reneolarte@yahoo.com>
2020-03-21 21:25:51 +0000

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
+		free (new_node);
 		return (NULL);
+	}
 	new_node->str  = strdup(str);
 	while (new_node->str[leng_nod] != '\0')
 		leng_nod++;
