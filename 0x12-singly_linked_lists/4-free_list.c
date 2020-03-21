0b1795ea81a0cc6d5cb075da53ff27f4abe62e17
mauricioolarte <reneolarte@yahoo.com>
2020-03-21 23:19:27 +0000

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *void free_list - free nodes.
+ *@head: is node.
+ */
+
+void free_list(list_t *head)
+{
+
+	while (head->next != NULL)
+	{
+		free(head->str);
+		free(head->next);
+		free(head);
+		head = head->next;
+	}
+	free(head->str);
+	free(head->next);
+	free(head);
+}
