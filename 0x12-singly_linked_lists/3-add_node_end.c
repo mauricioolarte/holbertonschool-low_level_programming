879e5a4eb988e5013e27ac508f13dc1ad0d30898
mauricioolarte <reneolarte@yahoo.com>
2020-03-21 22:30:28 +0000

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,6 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	int stringlen = 0;
 	list_t *last = *head;
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 		free(new_node);
