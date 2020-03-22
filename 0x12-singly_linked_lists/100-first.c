7e36f5e707f1d30b24ce4ce1584aad31d532a413
mauricioolarte <reneolarte@yahoo.com>
2020-03-22 02:01:35 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,10 +5,11 @@
  *printbeforeMain - print before main.
  *Return: cero.
  */
-int printbeforeMain(void) __attribute__ ((constructor));
+int printbeforeMain(void)__attribute__ ((constructor));
+
 int printbeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore  my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore ");
+printf("my house upon my back!\n");
 	return (0);
 }
