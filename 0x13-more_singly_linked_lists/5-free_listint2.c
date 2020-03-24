8d81edd289bfaeea98ec64546661a66b11046943
mauricioolarte <reneolarte@yahoo.com>
2020-03-24 15:55:24 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 		return;
 
 	while ((*head)->next != NULL)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-		}
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
 	free(*head);
 	*head = NULL;
 }
