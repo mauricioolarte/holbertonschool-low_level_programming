0108941db883eecb3b8969a47511e482b02ad422
mauricioolarte <reneolarte@yahoo.com>
2020-03-25 03:33:52 +0000

    delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp = *head;
+	listint_t *next;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (-1);
+	}
+	for (i = 0; i < index-1; i++)
+		temp = temp->next;
+	if (temp == NULL || temp->next == NULL)
+		return (-1);
+	next = temp->next->next;
+	free(temp->next);
+	temp->next = next;
+	return (1);
+}
