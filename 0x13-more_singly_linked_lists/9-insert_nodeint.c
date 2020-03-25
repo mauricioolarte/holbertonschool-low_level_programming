a4ee7ed555ba7ac0f3db5294d91a22421e74ae67
mauricioolarte <reneolarte@yahoo.com>
2020-03-25 04:55:22 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,9 +3,10 @@
 #include "lists.h"
 
 /**
- *get_nodeint_at_index - print all of number int in a linked list.
+ *insert_nodeint_at_index - print all of number int in a linked list.
  *@head: is pointer to head of linked list.
- *@index: is integer.
+ *@idx: is integer.
+ *@n: data.
  *Return: quantity of nodes.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -15,13 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = (listint_t *)(malloc(sizeof(listint_t)));
 
 	new_node->n = n;
-	if (n == 0)
+	if (idx == 0)
 	{
-		new_node = (*head);
+		new_node->next = (*head);
 		(*head) = new_node;
 		return (new_node);
 	}
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx; i++)
 	{
 		temp = *head;
 		*head = (*head)->next;
