24f8195968da74ee8798a1e2c0b54b240dfbc578
mauricioolarte <reneolarte@yahoo.com>
2020-03-27 01:12:39 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
+/**
+ *print - print a binary.
+ *@b: is a integer.
+ */
+
+void print(unsigned long int b)
+{
+	unsigned int c;
+
+	if (b != 0)
+	{
+		print(b >> 1);
+		c = b - ((b >> 1) * 2);
+		_putchar(c + '0');
+	}
+}
+
+
 /**
  *print_binary - convert integer to binary.
  *@n: is a interger.
@@ -8,12 +27,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int c;
-
-	if (n != 0)
-	{
-		print_binary(n >> 1);
-		c = n - ((n >> 1) * 2);
-		_putchar(c + '0');
-	}
+	if (n == 0)
+		_putchar('0');
+	else
+		print(n);
 }
