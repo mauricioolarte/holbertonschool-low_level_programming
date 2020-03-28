c1357ab86d36925ce787f7faf4f17aa3054eecbc
mauricioolarte <reneolarte@yahoo.com>
2020-03-28 03:19:37 +0000

    5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,73 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-
 /**
- *binary_to_uint2 - convert binary in integer.
- *@b: is char.
- *Return: sum.
+ *countSetBits - count the bit to change
+ *@n: is integer.
+ *Return: count.
  */
-
-unsigned int binary_to_uint2(char *b)
+int countSetBits(int n)
 {
-	int count = 0, mul = 0, sum = 0, c;
+	int count = 0;
 
-	if (b == NULL)
-		return (0);
-	while (b[count] != '\0')
-		count++;
-	c = count;
-	while (count > 0)
-	{
-		if (count == c)
-			mul = 1;
-		else
-			mul = mul * 2;
-		if (b[count - 1] != '0' && b[count - 1] != '1')
-			return (0);
-		if (b[count - 1] == '1')
-		{
-			sum = sum + mul;
-		}
-		count--;
-	}
-	return (sum);
-}
-
-
-/**
- *flip_bits - convert integer to binary.
- *@n: is a interger.
- *@m: number to print.
- *Return: index.
- */
-
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	unsigned long int c, nueindex, i, j;
-	char *p;
-
-	c = n;
 	while (n)
 	{
-		n = (n / 2);
-		i++;
+		count += n & 1;
+		n >>= 1;
 	}
-	if (m > i)
-		i = m;
-	p = (char *)malloc(sizeof(char) * (i - 1));
-	if (p == NULL)
-	{
-		free(p);
-		return (-1);
-	}
-	for (j = i; j > 0; j--)
-	{
-		p[j] = c % 2;
-		c = c / 2;
-	}
+	return (count);
+}
 
-	nueindex = i - (m + 1);
-	p[nueindex] = 1;
-	return (1);
+/**
+ *flip_bits - count the bit to flip a to b.
+ *@n: integer
+ *@m: integer
+ *Return: integer.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+
+	return (countSetBits(n ^ m));
 }
