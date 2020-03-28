65f8187250333d3e0f6ca8fec804d4e316b75caa
mauricioolarte <reneolarte@yahoo.com>
2020-03-28 02:38:50 +0000

    #$

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,9 +15,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	if (index > 32)
 		return (-1);
-	if ((*n & (1 << index)) == 0)
-		return (1);
-	else
+	if ((*n & (1 << index)) != 0)
 		*n = *n ^ (1 << index);
 
 	return (1);
