b39e6c7b83bb5a57ab6f0861e96358cc8b293ceb
mauricioolarte <reneolarte@yahoo.com>
2020-03-31 23:04:56 +0000

    23

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 /* Creación y apertura del fichero */
-	fd1 = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd1 = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 /* Comprobación de errores */
 	if (fd1 < 0)
