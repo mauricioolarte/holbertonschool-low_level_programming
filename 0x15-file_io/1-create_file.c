94923375a4667dfdd299597c3d8db833021ef5dc
mauricioolarte <reneolarte@yahoo.com>
2020-03-31 22:33:08 +0000

    12

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ *create_file - create a file.
+ *@filename: is a file.
+ *@text_content: is string.
+ *Return: 0 is ok, -1 is wrong.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int vret, fd1;
+
+	if (filename == NULL)
+		return (-1);
+
+/* Creación y apertura del fichero */
+	fd1 = open(filename, O_CREAT | O_WRONLY, 0600);
+
+/* Comprobación de errores */
+	if (fd1 < 0)
+	{
+		perror("Error al abrir fichero:");
+		exit(1);
+	}
+
+/* Escritura de la cadena */
+	vret = write(fd1, text_content, strlen(text_content));
+	if (vret < 0)
+	{
+		perror("Error al escriber fichero:");
+		return (-1);
+	}
+	close(fd1);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
-#define _HOLBERON_H_
+#define _HOLBERTON_H_
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* _HOLBERTON_H_*/
