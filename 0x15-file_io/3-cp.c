60765dfc10ad4f68eab5694f672d766753c30404
mauricioolarte <reneolarte@yahoo.com>
2020-04-01 02:48:48 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,8 @@
 
 /**
  * main - check the code for Holberton School students.
- *
+ *@ac: number of arguments.
+ *@av: arguments.
  * Return: Always 0.
  */
 int main(int ac, char **av)
@@ -16,12 +17,11 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: %s %s\n", av[1], av[2]);
 		exit(97);
 	}
-/*	res = append_text_to_file(av[1], av[2]);
-	printf("-> %i)\n", res);*/
+	res = append_text_to_file(av[1], av[2]);
+	printf("-> %i)\n", res);
 	printf("%c%c", av[0][2], av[0][3]);
 	if (av[0][2] != 'c' || av[0][3] != 'p' || av[0][4] != '\0')
 		exit(0);
-	else
-		printf("no");
+
 	return (0);
 }
