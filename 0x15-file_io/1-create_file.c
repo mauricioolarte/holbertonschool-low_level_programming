45fcc2b6d039152ed799569828a3fc99404c4431
mauricioolarte <reneolarte@yahoo.com>
2020-04-01 18:59:06 +0000

    uu

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int vret, fd1;
 
-	if (filename[0] == 'N' && filename[1] == 'U' &&
-		filename[2] == 'L' && filename[3] == 'L')
+	if (open(filename, O_WRONLY) == -1)
 	{
 		return (-1);
 	}
@@ -25,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd1 < 0)
 	{
 		perror("Error al abrir fichero:");
-		exit(-1);
+		return (-1);
 	}
 
 /* Escritura de la cadena */
