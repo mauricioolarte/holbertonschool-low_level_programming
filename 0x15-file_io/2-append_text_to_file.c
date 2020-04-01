db53a8b5bb443a116aa23b3403d7d42e2b57b9f5
mauricioolarte <reneolarte@yahoo.com>
2020-04-01 01:18:25 +0000

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ *create_file - create a file.
+ *@filename: is a file.
+ *@text_content: is string.
+ *Return: 0 is ok, -1 is wrong.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int vret, fd1;
+
+	if (filename == NULL)
+		return (-1);
+	printf("1");
+/* Creación y apertura del fichero */
+	fd1 = open(filename, O_WRONLY | O_APPEND);
+	printf("hola");
+/* Comprobación de errores */
+	if (fd1 < 0)
+	{
+		perror("Error al abrir fichero:");
+		exit(1);
+	}
+	printf("estoyaquie");
+/* Escritura de la cadena */
+	vret = write(fd1, text_content, strlen(text_content));
+	printf("excribi");
+	if (vret < 0)
+	{
+		perror("Error al escriber fichero:");
+		return (-1);
+	}
+	close(fd1);
+	return (1);
+}
