8ab058dca92e96c4abb2593c354adc2c208558ec
mauricioolarte <reneolarte@yahoo.com>
2020-04-01 04:03:17 +0000

    23

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd1 < 0)
 	{
 		perror("Error al abrir fichero:");
-		exit(1);
+		retur (-1);
 	}
 /* Escritura de la cadena */
 	vret = write(fd1, text_content, strlen(text_content));
