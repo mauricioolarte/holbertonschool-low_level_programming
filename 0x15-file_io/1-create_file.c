c8429a5140ffa94dac7ae89ee130113a59ec179e
mauricioolarte <reneolarte@yahoo.com>
2020-04-02 01:04:30 +0000

    23

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int vret, fd1;
 
-	if (open(filename, O_WRONLY) == -1)
-	{
-		return (-1);
-	}
-/* Creación y apertura del fichero */
-	fd1 = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-/* Comprobación de errores */
+	// si text_content is null create a void file.
+	if (text_content == NULL)
+		text_content = "";
+	fd1 = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd1 < 0)
 	{
-		perror("Error al abrir fichero:");
 		return (-1);
 	}
 
-/* Escritura de la cadena */
 	vret = write(fd1, text_content, strlen(text_content));
 	if (vret < 0)
 	{
-		perror("Error al escriber fichero:");
 		return (-1);
 	}
 	close(fd1);
