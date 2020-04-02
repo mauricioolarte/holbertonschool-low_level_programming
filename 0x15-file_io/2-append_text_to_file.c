7a77fdffaf72d855558a0699fa2d9a53d7149158
mauricioolarte <reneolarte@yahoo.com>
2020-04-02 03:18:03 +0000

    hh

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,27 +11,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int vret, fd1;
+	int vret, fd1, vwrite;
 
 	if (filename == NULL)
 		return (-1);
-/* Creación y apertura del fichero */
-	fd1 = open(filename, O_WRONLY | O_APPEND, 0664);
-/* Comprobación de errores */
+
+	fd1 = open(filename, O_RDWR | O_APPEND, 0664);
+
 	if (fd1 < 0)
 	{
-		perror("Error al abrir fichero:");
 		return (-1);
 	}
-	if (text_content == NULL && fd1 == -1)
-		return (-1);
-	else
+	vwrite = strlen(text_content);
+	if (vwrite == 0)
 		return (1);
-/* Escritura de la cadena */
-	vret = write(fd1, text_content, strlen(text_content));
+	vret = write(fd1, text_content, vwrite);
 	if (vret < 0)
 	{
-		perror("Error al escriber fichero:");
 		return (-1);
 	}
 	close(fd1);
