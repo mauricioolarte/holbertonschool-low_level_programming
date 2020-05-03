871356e4d550295999372e3dd838422db1e5cc87
mauricioolarte <reneolarte@yahoo.com>
2020-05-03 13:38:22 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,23 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	return (NULL);
 }
 /**
+*dlistint_len - print all the nodes of h.
+*@h: is a double linked list head.
+*Return: number of nodes.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int number_nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		number_nodes++;
+	}
+	return (number_nodes);
+}
+
+/**
  **insert_dnodeint_at_index - returns the nth node of a linked list.
  *@h: is the head of linked list.
  *@idx: is the nth node to return.
@@ -32,14 +49,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *nodeidx;
+	unsigned int countnodes;
 
+	countnodes = dlistint_len(*h);
 	if (*h == NULL)
 	{
 		add_dnodeint(h, n);
 		return (*h);
 	}
+	if (idx == countnodes)
+	{
+		add_dnodeint_end(h, n);
+		return (*h);
+	}
 	nodeidx = get_dnodeint_at_index(*h, idx);
-
+	if (nodeidx == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
