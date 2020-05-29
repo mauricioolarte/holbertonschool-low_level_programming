4a89523e155cc6ee1f99d293dacda3d8d3e10641
mauricioolarte <reneolarte@yahoo.com>
2020-05-29 05:38:41 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,7 +1,5 @@
 #ifndef __HASH_TABLE__
 #define __HASH_TABLE__
-
-
 /* structures*/
 
 /**
@@ -14,9 +12,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -29,12 +27,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
-
-
-
 
 /* prototype of functions*/
 
@@ -47,4 +42,4 @@ void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 
-#endif 
+#endif
