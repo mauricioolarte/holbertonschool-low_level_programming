032a936893215d858ace4cf31e3fe965dccd2ea4
mauricioolarte <reneolarte@yahoo.com>
2020-05-30 01:20:09 +0000

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+#define ACTIVE 1
+/**
+ *hash_table_get - this is for include a value in hash table
+ *@ht: is address hast table
+ *@key: key of data
+ *Return: 0 is fail or 1 is succes
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	int flag = 0;
+	unsigned long int i;
+	hash_node_t *head;
+
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+
+		if (ht->array[i] != NULL)
+		{
+			head = malloc(sizeof(hash_node_t));
+			if (head == NULL)
+				return;
+			head = ht->array[i];
+			while (head)
+			{
+				if (flag == ACTIVE)
+					printf(", ");
+				printf("\'%s\': ", head->key);
+				printf("\'%s\'", head->value);
+				head = head->next;
+			}
+			free(head);
+			flag = ACTIVE;
+		}
+	}
+	printf("}\n");
+		return;
+}
