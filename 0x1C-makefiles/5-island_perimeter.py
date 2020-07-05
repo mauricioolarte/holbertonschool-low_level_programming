dc5782937f553312595ed3d7bf8f89647e7c55ff
mauricioolarte <reneolarte@yahoo.com>
2020-07-05 02:27:24 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""this module have a function to calculate the perimeter of a islan 
+"""this module have a function to calculate the perimeter of a islan
 into a grid"""
+
 
 def island_perimeter(grid):
     """ calculate the perimetr of a island"""
     if len(grid) <= 100 and len(grid[0]) <= 100 and len(grid) != len(grid[0]):
         perimetre = 0
-        for fil in range (len(grid)):
-            for col in range (len(grid[0])):
+        for fil in range(len(grid)):
+            for col in range(len(grid[0])):
                 if grid[fil][col] == 1:
-                    if  (fil - 1) < 0 or grid[fil - 1][col] == 0:
+                    if (fil - 1) < 0 or grid[fil - 1][col] == 0:
                         perimetre += 1
                     if (col - 1) < 0 or grid[fil][col - 1] == 0:
                         perimetre += 1
                     if (fil + 1) > (len(grid) - 1) or grid[fil + 1][col] == 0:
                         perimetre += 1
-                    if (col + 1) > (len(grid[0]) - 1) or grid[fil][col + 1] == 0:
+                    if (col + 1) > (
+                            len(grid[0]) - 1) or grid[fil][col + 1] == 0:
                         perimetre += 1
         return perimetre
