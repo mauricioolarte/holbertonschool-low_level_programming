c9988fe1f3ea12f89e3c4b7f0b68548e2281b6d7
mauricioolarte <reneolarte@yahoo.com>
2020-08-27 21:21:09 +0000

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,10 +25,6 @@ int jump_search(int *array, size_t size, int value)
 		r_idx = l_idx;
 		l_idx = jump + l_idx;
 		printf("Value checked array[%i] = [%i]\n", r_idx, array[r_idx]);
-		if (l_idx > size)
-		{
-			break;
-		}
 	}
 
 	printf("Value found between indexes [%i] and [%i]\n", r_idx, l_idx);
